fdd84c14294d12cf160566883d56ae1cd7cbbb48
darzwaitz <darzwaitz@yahoo.com>
2021-03-04 23:52:43 +0000

    fix for micromatch version TBU

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,36 @@
 var elixir = require('laravel-elixir');
 
+//tell elixir to not compile sourcemap
 elixir.config.sourcemaps = false;
 
 var gulp = require('gulp');
 
 elixir(function (mix) {
+
     //compile sass to css
     mix.sass('resources/assets/sass/app.scss', 'resources/assets/css');
 
     //combine all css files
     mix.styles(
         [
-            'css/app.css',
+            'css/app.css', //creates 'css' destination folder for the file
             'bower/vendor/slick-carousal/slick/slick.css'
 
         ],'public/css/all.css',//output file
         'resources/assets');
 
-        var bowerPath = 'bower/vendor';
+    var bowerPath = 'bower/vendor';
 
-        mix.scripts(
-        [
-            //jquery
-            bowerPath + '/jquery/dist/jquery.min.js',
+    mix.scripts(
+    [
+        //jquery
+        bowerPath + '/jquery/dist/jquery.min.js',
 
-            //foundation js
-            bowerPath + '/foundation-sites/js/foundation.min.js',
+        //foundation js
+        bowerPath + '/foundation-sites/dist/js/foundation.min.js',
 
-            //other dependencies
-            bowerPath + '/slick-carousal/slick/slick.min.js'
-        ],'public/js/all.js', 'resources/assets'
-        );
+        //other dependencies
+        bowerPath + '/slick-carousal/slick/slick.min.js'
+    ],'public/js/all.js', 'resources/assets' //create ouput file, location of files to be combined
+    );
 });
